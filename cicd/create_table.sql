4d4a2d1963b8657ed77f86b51b79ae381445752d
kevin.vaningen@codecentric.nl <hjk12uio>
2020-09-24 13:28:48 +0200

    create table

--- cicd/create_table.sql
+++ cicd/create_table.sql
@@ -5,6 +5,6 @@ create table domain_event (
     eventSequenceNumber bigint not null,
     payload text not null,
     payload_type varchar(255) not null,
-    time_stamp varchar(255) not null,
+    timeStamp varchar(255) not null,
     primary key (eventIdentifier)
 );
