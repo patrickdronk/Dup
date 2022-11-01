0d9b9a4a66636dc7c9d1b17f588d7c7e981bb20f
schellingv <v.schelling@portofrotterdam.com>
2022-11-01 11:57:39 +0100

    Add custom event bus with policy

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import {App, Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import * as events from "aws-cdk-lib/aws-events"
+import {CfnEventBusPolicy, EventBus} from "aws-cdk-lib/aws-events";
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
@@ -9,7 +11,41 @@ export class MyStack extends Stack {
     new NodejsFunction(this, 'MyFunction', {
       entry: 'src/app/test.ts',
       handler: 'handler',
-    })
+    });
+
+    const customEventBus = new EventBus(this, "CustomEventBus", {
+      eventBusName: "customer-subscription-bus"
+    });
+
+    //Adding a resource based policy to custom event bus
+    new CfnEventBusPolicy(this,"EventBusResourcePolicy", {
+      statementId: "CustomerSubscriptionSid",
+      eventBusName: customEventBus.eventBusName,
+      statement:
+          {
+            "Effect": "Allow",
+            "Action": [
+              "events:PutEvents"
+            ],
+            "Principal": {
+              "AWS": this.account
+            },
+            "Resource": customEventBus.eventBusArn,
+            "Condition": {
+              "StringEquals": {
+                "events:detail-type": "customer-subscription",
+                "events:source": "com.duleendra.customerapp"
+              }
+            }
+          }
+    });
+
+    /*
+    new events.Rule(this, "fiveMinuteRule", {
+      schedule: events.Schedule.cron({ minute: "0/5" }),
+    });
+    
+     */
   }
 }
 
