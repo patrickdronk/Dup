30f155570ba056b27a3f13e0ee6f644b6ab77d2f
MitchWijt <mitchel@wijt.net>
2022-11-02 13:20:07 +0100

    deploy

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@
     normally commands will be received via an API, however for now we will manually invoke our lib with this index.ts
  */
 
-import { uuid } from 'uuidv4';
+// import { uuid } from 'uuidv4';
 import { CreateBankAccountCommand, DepositCommand, WithdrawalCommand } from './app/bankAccount/commands';
 import { CommandBus } from './dup/bus/command-bus';
 import { CommandProcessor } from './dup/commandProcessor';
+import { ICommand } from './dup/command/command';
 
 const commandBus = new CommandBus();
 new CommandProcessor(commandBus);
@@ -17,12 +18,24 @@ const timeout = (ms: number) => {
 };
 
 //fixme
-export const work = async () => {
-  const aggregateId = uuid();
-  commandBus.dispatch('CreateBankAccountCommand', new CreateBankAccountCommand(aggregateId));
+export const work = async (event: any) => {
+  // const aggregateId = uuid();
+
+  const command = getCommand(event.commandName, event.command)
+  commandBus.dispatch(event.commandName, command);
   await timeout(2000);
-  commandBus.dispatch('DepositCommand', new DepositCommand(aggregateId, 50));
-  await timeout(2000);
-  commandBus.dispatch('WithdrawalCommand', new WithdrawalCommand(aggregateId, 20));
-  await timeout(2000);
+
+  // commandBus.dispatch('DepositCommand', new DepositCommand(aggregateId, 50));
+  // await timeout(2000);
+  // commandBus.dispatch('WithdrawalCommand', new WithdrawalCommand(aggregateId, 20));
+  // await timeout(2000);
 };
+
+const getCommand = (commandName: string, command: any): ICommand => {
+  switch(commandName){
+    case "CreateBankAccountCommand": return new CreateBankAccountCommand(command.aggregateId)
+    case "DepositCommand": return new DepositCommand(command.aggregateId, command.amount)
+    case "WithdrawalCommand": return new WithdrawalCommand(command.aggregateId, command.amount)
+    default: return new CreateBankAccountCommand(command.aggregateId)
+  }
+}
