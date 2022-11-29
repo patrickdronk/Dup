8d9e55e21a562c8696a17c40ea195dfad5e620ca
Patrick Dronk <patrickdronk@me.com>
2022-11-29 22:13:25 +0100

    Add the processor

--- src/app/server.ts
+++ src/app/server.ts
@@ -5,7 +5,7 @@ import { createSchema } from 'graphql-yoga';
 import { CommandBus } from '../dup/bus/command-bus';
 import { CommandProcessor } from '../dup/commandProcessor';
 import { CreateBankAccountCommand, DepositCommand, WithdrawalCommand } from './bankAccount/commands';
-import { BankAccountProjection } from './bankAccount/processors/bankaccountProjection';
+import { BankAccountProjection } from './bankAccount/processors/bankaccountProjection.processor';
 
 const commandBus = new CommandBus();
 new CommandProcessor(commandBus);
